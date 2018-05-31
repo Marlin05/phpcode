4372cc67badde88cc92f443d26f56f667d65a8f5
Marlin05 <livadii17@gmail.com>
2018-06-01 03:39:08 +0500

    git commit "add"

---
+++ fdir38/index.php
@@ -0,0 +1,22 @@
+<?php
+    mkdir('new dir');
+    rmdir('new dir');
+    
+    $arr = glob('*.php');
+    print_r($arr);
+    
+    function printDir($folder, $space = '') {
+        $files = scandir($folder);
+        foreach ($files as $file) {
+            if ($file == '.' || $file == '..') continue;
+            $f = $folder.'/'.$file;
+            echo $space.$file.'<br />';
+            if (is_dir($f)) printDir($f, $space.'&nbsp;&nbsp;');
+        }
+    }
+    echo '<br />';
+    //printDir('W:\modules\conemu');
+    
+    
+   // echo file_exists('index.php');
+?>
