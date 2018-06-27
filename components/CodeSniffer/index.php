45783c5983015bfe3f43802a9e73f088fc886a7c
Marlin05 <livadii17@gmail.com>
2018-06-27 08:32:50 +0500

    git add *
    add

---
+++ components/CodeSniffer/index.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+    * My script
+    * PHP version 7
+    *
+    * @category Class
+    * @package  PackageName
+    * @author   Marlin05 <livadii17@gmail.com>
+    * @license  https://github.com/Marlin05 PHP License
+    * @link     https://github.com/Marlin05
+    */
+    $x = 5;
+
+    // phpcs url.index.php
+?>
