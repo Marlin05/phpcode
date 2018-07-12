925f9cd961883a741a6a5328c5ad044f2554daf3
Marlin05 <livadii17@gmail.com>
2018-07-12 16:43:49 +0500

    git add *
    add

--- scripts/blocks/banner.php
+++
@@ -1,3 +0,0 @@
-<div class="banner"><?php echo $code;?></div>
-		
-<hr/>
--- scripts/blocks/banners_240.php
+++
@@ -1,8 +0,0 @@
-	<h2>Реклама</h2>
-	<?php
-	$banners = getAllBanners();
-	for ($i = 0; $i < count($banners); $i++) {
-		$code = $banners[$i] ["code"];
-		include "banner.php";
-	} 
-	?>
