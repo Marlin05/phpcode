4ab5c645f795ad4d4f395ea58b951391497e7235
Marlin05 <livadii17@gmail.com>
2018-08-16 07:24:29 +0500

    git add*

---
+++ BDMSQL/queries/12/queries.php
@@ -0,0 +1,62 @@
+﻿-- JOIN
+-- INNER JOIN
+-- LEFT OUTER JOIN
+-- RIGHT OUTER JOIN
+-- !FULL OUTER JOIN!
+-- CROSS JOIN
+
+-- 1
+SELECT * FROM salers INNER JOIN customers ON customers.snum = salers.snum;
+
+-- 2
+SELECT * FROM salers JOIN customers ON customers.snum = salers.snum;
+
+-- 3
+-- выбрать нужные поля в запросе
+-- объединение только на основе справочной целостности т.е. берутся только связанные ряды
+SELECT s.sname, c.cname, s.city FROM salers s INNER JOIN customers c ON c.snum = s.snum;
+
+-- 4
+SELECT s.sname, c.cname, s.city FROM salers s, customers c WHERE c.snum = s.snum; 
+
+-- 5
+SELECT s.sname, c.cname, s.city FROM salers s INNER JOIN customers c ON c.snum = s.snum WHERE s.city = 'London';
+
+-- 6
+SELECT s.sname, c.cname, s.city FROM salers s LEFT OUTER JOIN customers c ON c.snum = s.snum;
+
+-- 7
+SELECT s.sname, c.cname, s.city FROM salers s RIGHT OUTER JOIN customers c ON c.snum = s.snum;
+
+-- 8 ERROR!
+SELECT s.sname, c.cname, s.city FROM salers s FULL OUTER JOIN customers c ON c.snum = s.snum;
+
+-- 9 
+-- эмулирование FULL 
+-- выдаёт и левые и правые NULL без повторений
+SELECT s.sname, c.cname, s.city FROM salers s LEFT OUTER JOIN customers c ON c.snum = s.snum
+UNION
+SELECT s.sname, c.cname, s.city FROM salers s RIGHT OUTER JOIN customers c ON c.snum = s.snum;
+
+-- 10
+SELECT s.sname, c.cname, s.city FROM salers s CROSS JOIN customers c;
+
+-- 11 Полнотекстовый поиск
+SELECT * FROM customers WHERE MATCH (text) AGAINST ('test');
+
+-- 12 Полнотекстовый поиск в режиме естественного языка (NATURAL LANGUAGE MODE) морфология не поддерживается
+SELECT * FROM customers WHERE MATCH (text) AGAINST ('программирование');
+
+-- 13 Полнотекстовый поиск... в режиме NATURAL LANGUAGE MODE имеется порог шумовых слов = 50%
+SELECT * FROM customers WHERE MATCH (text) AGAINST ('mysql');
+
+-- 14
+SELECT * FROM customers WHERE MATCH (text) AGAINST ('php' IN NATURAL LANGUAGE MODE);
+
+-- 15 Полнотекстовый поиск в булевом режиме (BOOLEAN MODE) поддерживается морфология
+SELECT * FROM customers WHERE MATCH (text) AGAINST ('программ*' IN BOOLEAN MODE);
+
+-- 16
+SELECT * FROM customers WHERE MATCH (text) AGAINST ('+php -mysql' IN BOOLEAN MODE);
+
+-- ft_min_word_len=3
---
+++ BDMSQL/queries/13/index.php
@@ -0,0 +1,80 @@
+﻿<?php 
+
+@mysql_connect("localhost", "root", "") or die("Нет соединения с сервером");
+mysql_query("SET NAMES 'utf8'") or die("Не установлена кодировка соединения");
+mysql_select_db("sql2") or die("Не выбрана БД");
+
+/*
+$query = "INSERT INTO customers(cname, city, rating, snum, text) VALUES ('Вася Пупкин', 'Васюки', 150, 1002, 'Какой-то текст')";
+mysql_query($query);
+if(mysql_affected_rows()){
+	echo '<p>Данные добавлены</p>';
+}else{
+	echo '<p>Ошибка</p>';
+}
+*/
+/*
+$query = "UPDATE customers SET cname='Петров', city='LA' WHERE cnum=2009";
+mysql_query($query);
+if(mysql_affected_rows()){
+	echo '<p>Данные обновлены</p>';
+}else{
+	echo '<p>Ошибка</p>';
+}
+*/
+//удалить все стр.> 2009
+$query = "DELETE FROM customers WHERE cnum > 2009";
+mysql_query($query);
+if(mysql_affected_rows()){
+	echo '<p>Данные удалены</p>';
+}else{
+	echo '<p>Ничего не удалено</p>';
+}
+
+
+$query = "SELECT * FROM customers";
+$res = mysql_query($query) or die(mysql_error());
+
+
+//$row = mysql_fetch_assoc($res);
+//print_r($row);
+
+/* echo 
+Array
+(
+    [cnum] => 2001
+    [cname] => Hoffman
+    [city] => London
+    [rating] => 100
+    [snum] => 1001
+    [text] => php
+)
+*/
+
+/* вывести весь массив
+*/
+$data = array();
+while($row = mysql_fetch_assoc($res)){
+	$data[] = $row;
+}
+//print_r($data);
+
+//вывести 1 ячейку
+echo $data[2]['cname']. ' - ' . $data[2]['city'];
+
+?>
+<-- вывести таблицой res -->
+<table border="1">  
+
+<?php foreach($data as $item) :?>
+
+<tr>
+	<td><?=$item['cnum']?></td>
+	<td><?=$item['cname']?></td>
+	<td><?=$item['city']?></td>
+	<td><?=$item['rating']?></td>
+	<td><?=$item['snum']?></td>
+	<td><?=$item['text']?></td>
+</tr>  
+ <?php endforeach; ?>
+</table>
